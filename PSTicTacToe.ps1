bab41c8857e89bcfcc99e8ac7c3bba2c8469dcb3
Lazer5L <brenteri@gmail.com>
2017-03-11 15:28:27 -0600

    Eliminate Switch logic
    Removed switch statement by adding the Square array to locate all points
    on the board.

--- PSTicTacToe.ps1
+++ PSTicTacToe.ps1
@@ -1,6 +1,8 @@
 ﻿# PSTicTacToe.ps1
 # Brent Erickson
 
+$Square = @(25,27,29,13,15,17,1,3,5)
+
 Function ResetBoard { # Set playing board to default values
     $Board = "`r`n", "7", "|","8", "|", "9", "`r`n"
     $Board += "-", "-", "-", "-", "-", "`r`n"
@@ -12,20 +14,19 @@ Function ResetBoard { # Set playing board to default values
 
 Function CheckWin($Board, $Player) { # Check if current player has won
     $Array = @()
-    $Spot = 0 # Counter used for location in Board array
+    #$Spot = 0 # Counter used for location in Board array
     $Win = $False
-    foreach ($Item in $Board) { # Loop to find players mark
-        If ($Item -eq $Player) { # If players mark is found make note of the location
-            $Array += $Spot
+    foreach ($Item in $Square) { # Loop to find players mark
+        If ($Board[$Item] -eq $Player) { # If players mark is found make note of the location
+            $Array += $Item
         }
-        $Spot++
     }
     
     # Check all possible winning combinations
     if (($Array -contains "1") -and ($Array -contains "15") -and ($Array -contains"29")) { $Win = $True }
     if (($Array -contains "25") -and ($Array -contains "15") -and ($Array -contains"5")) { $Win = $True }
     if (($Array -contains "1") -and ($Array -contains "13") -and ($Array -contains"25")) { $Win = $True }
-    if (($Array -contains "3") -and ($Array -contains "15") -and ($Array -contains"25")) { $Win = $True }
+    if (($Array -contains "3") -and ($Array -contains "15") -and ($Array -contains"27")) { $Win = $True }
     if (($Array -contains "5") -and ($Array -contains "17") -and ($Array -contains"29")) { $Win = $True }
     if (($Array -contains "1") -and ($Array -contains "3") -and ($Array -contains"5")) { $Win = $True }
     if (($Array -contains "13") -and ($Array -contains "15") -and ($Array -contains"17")) { $Win = $True }
@@ -37,7 +38,7 @@ $Board = ResetBoard
 $Turn = 0 # Counter used to determine whose turn it is
 While ($Turn -le 8)
 {
-    clear-host
+    #clear-host
     write-host "`r`nTic Tac Toe`r`n" $Board
     If([bool]!($Turn%2)) { # Check Which Players turn it is
         $Player = "X"
@@ -45,72 +46,12 @@ While ($Turn -le 8)
     Else { 
         $Player = "O" 
     }
-    $Input = Read-host -prompt "$Player Which Square"
-    Switch ($Input) {
-        1 {
-            $Spot = 25 
-            If ($Board[$Spot] -eq $Input) { # If the square is still its default value player can take it
-                $Board[$Spot] = $Player
-                $Turn++
-            }
-        }
-        2 { 
-            $Spot = 27
-            If ($Board[$Spot] -eq $Input) {
-                $Board[$Spot] = $Player
-                $Turn++
-            } 
-        }
-        3 { 
-            $Spot = 29
-            If ($Board[$Spot] -eq $Input) {
-                $Board[$Spot] = $Player
-                $Turn++
-            }
-        }
-        4 { 
-            $Spot = 13
-            If ($Board[$Spot] -eq $Input) {
-                $Board[$Spot] = $Player
-                $Turn++
-            } 
-        }
-        5 { 
-            $Spot = 15
-            If ($Board[$Spot] -eq $Input) {
-                $Board[$Spot] = $Player
-                $Turn++
-            } 
-        }
-        6 { 
-            $Spot = 17
-            If ($Board[$Spot] -eq $Input) {
-                $Board[$Spot] = $Player
-                $Turn++
-            }  
-        }
-        7 { 
-            $Spot = 1
-            If ($Board[$Spot] -eq $Input) {
-                $Board[$Spot] = $Player
-                $Turn++
-            } 
-        }
-        8 { 
-            $Spot = 3
-            If ($Board[$Spot] -eq $Input) {
-                $Board[$Spot] = $Player
-                $Turn++
-            } 
-        }
-        9 { 
-            $Spot = 5
-            If ($Board[$Spot] -eq $Input) {
-                $Board[$Spot] = $Player
-                $Turn++
-            } 
-        }
+    [int]$Input = Read-host -prompt "$Player Which Square"
+    If ($Board[$Square[($Input - 1)]] -eq $Input) { # Compare Square array to Board using input as the position locator
+        $Board[$Square[($Input - 1)]] = $Player # Set that square as the players mark
+        $Turn++
     }
+
     if ((CheckWin $Board $Player)-eq $True) { $Turn = 90 } # If Returns true Player won by three in a row
 }
 If ($Turn -eq 90) { # Check if you win by Three in a row or if cats game
